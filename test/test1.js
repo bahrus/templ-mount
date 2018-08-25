1d7d0ac1a3139bc8d6683556e5b816b927bd8abf
bahrus <anderson.bruce.b@gmail.com>
2018-08-25 12:12:09 -0400

    more typesafe

--- test/test1.js
+++ test/test1.js
@@ -9,16 +9,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const test = require('tape');
 const puppeteer = require('puppeteer');
 const path = require('path');
-// function delay(time) {
-//     return new Promise(function(resolve) { 
-//         setTimeout(resolve, time)
-//     });
-//  }
+const TapeTestRunner = {
+    test: test
+};
 (() => __awaiter(this, void 0, void 0, function* () {
-    const browser = yield puppeteer.launch({
+    const launchOptions = {
         headless: true,
         args: ['--allow-file-access-from-files']
-    });
+    };
+    const browser = yield puppeteer.launch(launchOptions);
     const page = yield browser.newPage();
     page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
     const devFile = path.resolve(__dirname, '../demo/page-1c.html');
@@ -28,7 +27,7 @@ const path = require('path');
     const textContent = yield page.$eval('page-2c', (c) => c.shadowRoot.querySelector('page-3c'));
     yield page.screenshot({ path: 'example.png' });
     yield browser.close();
-    test('testing dev.html', (t) => {
+    TapeTestRunner.test('testing dev.html', t => {
         t.ok(textContent);
         t.end();
     });
--- test/test1.ts
+++ test/test1.ts
@@ -1,18 +1,19 @@
-import { ConsoleMessage } from "puppeteer";
+import { ConsoleMessage, Browser, LaunchOptions  } from "puppeteer"; //typescript
+import {Test} from "tape";
 
 const test = require('tape');
 const puppeteer = require('puppeteer');
 const path = require('path');
-// function delay(time) {
-//     return new Promise(function(resolve) { 
-//         setTimeout(resolve, time)
-//     });
-//  }
+const TapeTestRunner = {
+    test: test
+} as Test;
+
 (async () => {
-    const browser = await puppeteer.launch({
+    const launchOptions = {
         headless: true,
         args:['--allow-file-access-from-files']
-    });
+    } as LaunchOptions;
+    const browser = await puppeteer.launch(launchOptions) as Browser;
     const page = await browser.newPage();
     page.on('console', (msg: ConsoleMessage) => console.log('PAGE LOG:', msg.text()));
     const devFile = path.resolve(__dirname, '../demo/page-1c.html');
@@ -22,7 +23,7 @@ const path = require('path');
     const textContent = await page.$eval('page-2c', (c: any) => c.shadowRoot.querySelector('page-3c'));
     await page.screenshot({path: 'example.png'});
     await browser.close();
-    test('testing dev.html', (t: any) => {
+    TapeTestRunner.test('testing dev.html', t => {
         t.ok(textContent);
         t.end();
     });
