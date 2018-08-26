26ecfb1842ec8d89f22698f8ad32ff1df0b89dc6
bahrus <anderson.bruce.b@gmail.com>
2018-08-25 20:41:34 -0400

    ...

--- test/test1.js
+++ test/test1.js
@@ -6,30 +6,49 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+//import {runTests} from 'xtal-test/index.js';
+const xt = require('xtal-test/index');
 const test = require('tape');
-const puppeteer = require('puppeteer');
-const path = require('path');
-const TapeTestRunner = {
-    test: test
-};
-(() => __awaiter(this, void 0, void 0, function* () {
-    const launchOptions = {
-        headless: true,
-        args: ['--allow-file-access-from-files']
-    };
-    const browser = yield puppeteer.launch(launchOptions);
-    const page = yield browser.newPage();
-    page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
-    const devFile = path.resolve(__dirname, '../demo/page-1c.html');
-    yield page.goto(devFile);
-    //await delay(4000);
-    yield page.waitFor(4000);
-    const textContent = yield page.$eval('page-2c', (c) => c.shadowRoot.querySelector('page-3c'));
-    yield page.screenshot({ path: 'example.png' });
-    yield browser.close();
-    TapeTestRunner.test('testing dev.html', t => {
-        t.ok(textContent);
-        t.end();
+console.log(xt);
+function customTests(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log('doing customTests, wait 4 seconds');
+        yield page.waitFor(4000);
+        console.log('4 seconds passed');
+        const textContent = yield page.$eval('page-2c', (c) => c.shadowRoot.querySelector('page-3c'));
+        const TapeTestRunner = {
+            test: test
+        };
+        TapeTestRunner.test('testing dev.html', t => {
+            t.ok(textContent);
+            t.end();
+        });
     });
+}
+(() => __awaiter(this, void 0, void 0, function* () {
+    yield xt.runTests('demo/page-1c.html', customTests);
 }))();
+// const test = require('tape');
+// const puppeteer = require('puppeteer');
+// const path = require('path');
+// (async () => {
+//     const launchOptions = {
+//         headless: true,
+//         args:['--allow-file-access-from-files']
+//     } as LaunchOptions;
+//     const browser = await puppeteer.launch(launchOptions) as Browser;
+//     const page = await browser.newPage();
+//     page.on('console', (msg: ConsoleMessage) => console.log('PAGE LOG:', msg.text()));
+//     const devFile = path.resolve(__dirname, '../demo/page-1c.html');
+//     await page.goto(devFile);
+//     //await delay(4000);
+//     await page.waitFor(4000);
+//     const textContent = await page.$eval('page-2c', (c: any) => c.shadowRoot.querySelector('page-3c'));
+//     await page.screenshot({path: 'example.png'});
+//     await browser.close();
+//     TapeTestRunner.test('testing dev.html', t => {
+//         t.ok(textContent);
+//         t.end();
+//     });
+//   })();
 //# sourceMappingURL=test1.js.map
--- test/test1.ts
+++ test/test1.ts
@@ -1,31 +1,49 @@
-import { ConsoleMessage, Browser, LaunchOptions  } from "puppeteer"; //typescript
-import {Test} from "tape";
-
+//import {runTests} from 'xtal-test/index.js';
+const xt = require('xtal-test/index') as any;
 const test = require('tape');
-const puppeteer = require('puppeteer');
-const path = require('path');
-const TapeTestRunner = {
-    test: test
-} as Test;
-
-(async () => {
-    const launchOptions = {
-        headless: true,
-        args:['--allow-file-access-from-files']
-    } as LaunchOptions;
-    const browser = await puppeteer.launch(launchOptions) as Browser;
-    const page = await browser.newPage();
-    page.on('console', (msg: ConsoleMessage) => console.log('PAGE LOG:', msg.text()));
-    const devFile = path.resolve(__dirname, '../demo/page-1c.html');
-    await page.goto(devFile);
-    //await delay(4000);
+console.log(xt);
+import { ConsoleMessage, Browser, LaunchOptions, Page } from "puppeteer"; //typescript
+import { Test } from "tape";
+async function customTests(page: Page) {
+    console.log('doing customTests, wait 4 seconds');
     await page.waitFor(4000);
+    console.log('4 seconds passed');
     const textContent = await page.$eval('page-2c', (c: any) => c.shadowRoot.querySelector('page-3c'));
-    await page.screenshot({path: 'example.png'});
-    await browser.close();
+    const TapeTestRunner = {
+        test: test
+    } as Test;
     TapeTestRunner.test('testing dev.html', t => {
         t.ok(textContent);
         t.end();
     });
-    
-  })();
+}
+(async () => {
+    await  xt.runTests('demo/page-1c.html', customTests);
+})();
+
+// const test = require('tape');
+// const puppeteer = require('puppeteer');
+// const path = require('path');
+
+
+// (async () => {
+//     const launchOptions = {
+//         headless: true,
+//         args:['--allow-file-access-from-files']
+//     } as LaunchOptions;
+//     const browser = await puppeteer.launch(launchOptions) as Browser;
+//     const page = await browser.newPage();
+//     page.on('console', (msg: ConsoleMessage) => console.log('PAGE LOG:', msg.text()));
+//     const devFile = path.resolve(__dirname, '../demo/page-1c.html');
+//     await page.goto(devFile);
+//     //await delay(4000);
+//     await page.waitFor(4000);
+//     const textContent = await page.$eval('page-2c', (c: any) => c.shadowRoot.querySelector('page-3c'));
+//     await page.screenshot({path: 'example.png'});
+//     await browser.close();
+//     TapeTestRunner.test('testing dev.html', t => {
+//         t.ok(textContent);
+//         t.end();
+//     });
+
+//   })();
