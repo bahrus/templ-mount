c6bc9067b9bd588076ef3a3d2a71ee9821518d70
bahrus <anderson.bruce.b@gmail.com>
2018-08-26 14:56:09 -0400

    ...

--- test/test1.js
+++ test/test1.js
@@ -1,30 +1,20 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const xt = require('xtal-test/index');
 const test = require('tape');
-function customTests(page) {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.log('doing customTests, wait 4 seconds');
-        yield page.waitFor(4000);
-        const textContent = yield page.$eval('page-2c', (c) => c.shadowRoot.querySelector('page-3c'));
-        const TapeTestRunner = {
-            test: test
-        };
-        TapeTestRunner.test('testing dev.html', t => {
-            t.ok(textContent);
-            t.end();
-        });
+async function customTests(page) {
+    console.log('doing customTests, wait 4 seconds');
+    await page.waitFor(4000);
+    const textContent = await page.$eval('page-2c', (c) => c.shadowRoot.querySelector('page-3c'));
+    const TapeTestRunner = {
+        test: test
+    };
+    TapeTestRunner.test('testing dev.html', t => {
+        t.ok(textContent);
+        t.end();
     });
 }
-(() => __awaiter(this, void 0, void 0, function* () {
-    yield xt.runTests({
+(async () => {
+    await xt.runTests({
         path: 'test/page-1c.html'
     }, customTests);
-}))();
+})();
 //# sourceMappingURL=test1.js.map
