28cf4722caba0da79b186d4325a3698e8e3f60cd
bahrus <anderson.bruce.b@gmail.com>
2018-08-26 08:47:00 -0400

    ...

--- test/test1.js
+++ test/test1.js
@@ -8,12 +8,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const xt = require('xtal-test/index');
 const test = require('tape');
-console.log(xt);
 function customTests(page) {
     return __awaiter(this, void 0, void 0, function* () {
         console.log('doing customTests, wait 4 seconds');
         yield page.waitFor(4000);
-        console.log('4 seconds passed');
         const textContent = yield page.$eval('page-2c', (c) => c.shadowRoot.querySelector('page-3c'));
         const TapeTestRunner = {
             test: test
--- test/test1.ts
+++ test/test1.ts
@@ -1,13 +1,11 @@
-import {IXtalTestRunner, IXtalTestRunnerOptions} from 'xtal-test/index.js';
+import {IXtalTestRunner} from 'xtal-test/index.js';
 const xt = require('xtal-test/index') as IXtalTestRunner;
 const test = require('tape');
-console.log(xt);
-import { ConsoleMessage, Browser, LaunchOptions, Page } from "puppeteer"; //typescript
+import { Page } from "puppeteer"; //typescript
 import { Test } from "tape";
 async function customTests(page: Page) {
     console.log('doing customTests, wait 4 seconds');
     await page.waitFor(4000);
-    console.log('4 seconds passed');
     const textContent = await page.$eval('page-2c', (c: any) => c.shadowRoot.querySelector('page-3c'));
     const TapeTestRunner = {
         test: test
