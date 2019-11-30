9ed74500688af66517dc259bb9e66de450f0389a
bahrus <anderson.bruce.b@gmail.com>
2019-11-30 10:32:38 -0500

    ...

--- first-templ.js
+++ first-templ.js
@@ -26,12 +26,25 @@ export class FirstTempl {
                         tm: this.tm,
                         template: t
                     }).then(val => {
-                        e.detail.value.appendChild(val.content.cloneNode(true));
+                        const ioi = {
+                            threshold: 0.01
+                        };
+                        const observer = new IntersectionObserver(this.callback.bind(this), ioi);
+                        observer.observe(e.detail.value);
+                        //(<any>e).detail.value.appendChild((<any>val).content.cloneNode(true));
                     });
                 });
                 this.tm.appendChild(impTObserver);
             }
         });
         this.tm.appendChild(templateObserver);
+    }
+    async callback(entries, observer) {
+        const first = entries[0];
+        if (first.intersectionRatio > 0) {
+            const templURL = first.target.getAttribute('imp-t');
+            const template = await TemplMount.template(templURL, { tm: this.tm });
+            first.target.appendChild(template.content.cloneNode(true));
+        }
     }
 }
--- first-templ.ts
+++ first-templ.ts
@@ -2,6 +2,14 @@ import {CssObserve} from 'css-observe/css-observe.js';
 import {TemplMount} from './templ-mount.js';  
 
 export class FirstTempl{
+    async callback(entries: any, observer: any){
+        const first = entries[0];
+        if(first.intersectionRatio > 0){
+            const templURL = first.target.getAttribute('imp-t');
+            const template = await TemplMount.template(templURL, {tm: this.tm}) as HTMLTemplateElement;
+            first.target.appendChild(template.content.cloneNode(true));
+        }
+    }
     constructor(public tm: TemplMount){
         const templateObserver = document.createElement(CssObserve.is) as CssObserve;
         templateObserver.observe = true;
@@ -27,7 +35,12 @@ export class FirstTempl{
                         tm: this.tm,
                         template: t   
                     }).then(val =>{
-                        (<any>e).detail.value.appendChild((<any>val).content.cloneNode(true));
+                        const ioi : IntersectionObserverInit = {
+                            threshold: 0.01
+                        };
+                        const observer = new IntersectionObserver(this.callback.bind(this), ioi);
+                        observer.observe((<any>e).detail.value);
+                        //(<any>e).detail.value.appendChild((<any>val).content.cloneNode(true));
                     })
                     
                 });
