b67138e7bf86dbccbff32b5ba3a31f465dfd6c4f
Bruce Anderson <anderson.bruce.b@gmail.com>
2019-12-07 12:36:40 -0500

    ...

--- test/test-all.js
+++ test/test-all.js
@@ -3,7 +3,7 @@ const xt = require('xtal-test/index');
     const passed = await xt.runTests([
         {
             path: 'test/fly-mount.html',
-            expectedNoOfSuccessMarkers: 3,
+            expectedNoOfSuccessMarkers: 4,
         },
     ]);
     if (passed) {
